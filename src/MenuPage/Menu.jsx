f4a5728ea8119023a5220c1b1c0dda25d7b87310
rofikul-resan <resan6231@gmail.com>
2023-05-25 01:48:12 +0600

    complite menu page

--- src/MenuPage/Menu.jsx
+++ src/MenuPage/Menu.jsx
@@ -10,12 +10,23 @@ const Menu = () => {
   const [allMenu, setAllMenu] = useState([]);
   useDataFetch("http://localhost:5000/menu", setAllMenu);
   const offeredMenu = allMenu.filter((menu) => menu.category === "offered");
+  const dessertMenu = allMenu.filter((menu) => menu.category === "dessert");
+  const pizzaMenu = allMenu.filter((menu) => menu.category === "pizza");
+  const saladMenu = allMenu.filter((menu) => menu.category === "salad");
+  const soupMenu = allMenu.filter((menu) => menu.category === "soup");
   return (
     <div>
       <PageHeader bg={heroImg} title={"OUR MENU"} />
       <SectionHeader headerName={"Don't miss"} title={"TODAY'S OFFER"} />
       <CategoryFoodContener items={offeredMenu} />
       <MenuSectionHeader title={"DESSERTS"} />
+      <CategoryFoodContener items={dessertMenu} />
+      <MenuSectionHeader title={"pizza"} />
+      <CategoryFoodContener items={pizzaMenu} />
+      <MenuSectionHeader title={"salad"} />
+      <CategoryFoodContener items={saladMenu} />
+      <MenuSectionHeader title={"soup"} />
+      <CategoryFoodContener items={soupMenu} />
     </div>
   );
 };
--- src/MenuPage/MenuSectionHeader.jsx
+++ src/MenuPage/MenuSectionHeader.jsx
@@ -5,7 +5,7 @@ const MenuSectionHeader = ({ title }) => {
       className={` h-[600px] bg-contain grid place-items-center my-20 `}
     >
       <div className="w-8/12 mx-auto p-20 text-center bg-black/50 text-white ">
-        <h1 className="text-5xl font-cinzel font-bold">{title}</h1>
+        <h1 className="text-5xl font-cinzel font-bold uppercase">{title}</h1>
         <p>
           Lorem Ipsum has been the industry standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
