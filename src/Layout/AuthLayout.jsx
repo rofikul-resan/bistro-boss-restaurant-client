aacfb0f79d7afd819bcf437ff13bae39691cd9c0
rofikul-resan <resan6231@gmail.com>
2023-05-26 18:55:38 +0600

    add firebase

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import authBg from "../assets/reservation/wood-grain-pattern-gray1x.png";
 
 const AuthLayout = () => {
   return (
-    <div className="flex justify-center my-20">
+    <div
+      style={{ background: `url("${authBg}")` }}
+      className="flex py-20 min-h-screen"
+    >
       <div>
         <Outlet />
       </div>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../home/Home";
 import Menu from "../MenuPage/Menu";
 import ShopOrder from "../ShopOrderPage/ShopOrder";
+import AuthLayout from "../Layout/AuthLayout";
+import Login from "../AuthPage/Login";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,16 @@ const router = createBrowserRouter([
       {
         path: "/our-shop/:title",
         element: <ShopOrder />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
